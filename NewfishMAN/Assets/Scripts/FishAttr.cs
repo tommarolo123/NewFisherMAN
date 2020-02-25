11dfb640da046a83b156d0a9bb0818822665717f
tommarolo123 <tommarolo123@yahoo.co.jp>
2020-02-26 03:14:48 +0900

    魚群の生まれ点、魚のスピードと生まれ数ランダム生成

---
+++ NewfishMAN/Assets/Scripts/FishAttr.cs
@@ -0,0 +1,8 @@
+﻿
+using UnityEngine;
+
+public class FishAttr : MonoBehaviour
+{
+    public int maxNum;
+    public int maxSpeed; 
+}
---
+++ NewfishMAN/Assets/Scripts/FishMaker.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+public class FishMaker : MonoBehaviour
+{
+    public Transform fishHolder;//魚
+    public Transform[] genPositions; //魚が生まれ点
+    public GameObject[] fishPrafabs;//魚のprefab
+    
+
+    void MakeFishes()
+    {
+        int genPosIndex = Random.Range(0, genPositions.Length);//ランダムの位置から魚を生成する
+        int fishPreIndex = Random.Range(0, fishPrafabs.Length);//ランダムに魚を生成する
+                                                             
+        int maxNum = fishPrafabs[fishPreIndex].GetComponent<FishAttr>().maxNum;  //魚の最大生成量とスピード
+        int maxSpeed = fishPrafabs[fishPreIndex].GetComponent<FishAttr>().maxSpeed;
+        int num = Random.Range((maxNum / 2) + 1, maxNum);//生成される量が1から最大
+        int speed = Random.Range(maxSpeed / 2, maxSpeed);
+        int moveType = Random.Range(0, 2); //0の場合まっすぐ　2は曲がる
+        int angOffset;//まっすぐの場合　x軸との角度
+        int angSpeed;//曲がる角スピード
+        
+        if (moveType == 0)
+        {
+            //まっすぐ魚群
+        }
+        else
+        {
+            //曲がる
+        }
+    }
+
+}
