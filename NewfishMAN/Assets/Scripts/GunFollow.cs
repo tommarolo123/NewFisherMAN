8bf9849e7be91fea66bb777c32db6d1dc670c8dd
tommarolo123 <tommarolo123@yahoo.co.jp>
2020-02-26 01:52:59 +0900

    鉄砲がマウスの動きによる移動、魚、弾、ゴールドのPrefab作成

---
+++ NewfishMAN/Assets/Scripts/GunFollow.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class GunFollow : MonoBehaviour
+{
+    public RectTransform UGUICanvas;
+    public Camera mainCamera;
+    void Update()
+    {
+        Vector3 mousePos;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(UGUICanvas, new Vector2(Input.mousePosition.x,
+            Input.mousePosition.y), mainCamera, out mousePos);
+        //canvasの中マウスのポジション
+        float z;
+        if (mousePos.x>transform.position.x)
+        {
+            z = -Vector3.Angle(Vector3.up, mousePos - transform.position);//鉄砲の方向とマウスの角度
+        }
+        else
+        {
+            z = Vector3.Angle(Vector3.up, mousePos - transform.position);//鉄砲の方向とマウスの角度
+        }
+
+        transform.localRotation = Quaternion.Euler(0, 0, z); 
+    }
+}
