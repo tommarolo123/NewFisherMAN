08a50ef45be6247d1cd0250dff68532ec845fde7
tommarolo123 <tommarolo123@yahoo.co.jp>
2020-02-27 01:29:58 +0900

    まっすぐ動く魚の生成　エラーあり

---
+++ NewfishMAN/Assets/Scripts/Effect/Ef_AutoMove.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+public class Ef_AutoMove : MonoBehaviour
+{
+    public float speed = 1f;
+    public Vector3 Dir = Vector3.right;//x軸正方向
+    　
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Dir * speed*Time.deltaTime);//移動  
+    }
+}
--- NewfishMAN/Assets/Scripts/FishMaker.cs
+++ NewfishMAN/Assets/Scripts/FishMaker.cs
@@ -1,28 +1,40 @@
 ﻿
 using UnityEngine;
-
+using System.Collections;
 public class FishMaker : MonoBehaviour
 {
     public Transform fishHolder;//魚
     public Transform[] genPositions; //魚が生まれ点
-    public GameObject[] fishPrafabs;//魚のprefab
-    
+    public GameObject[] fishPrefabs;//魚のprefab
+    public float waveGenWaitTime = 0.3f; //0.3秒ごとに魚を生成する
+
+
+    void Start()
+    {
+        InvokeRepeating("MakeFishes", 0, waveGenWaitTime);
+
+    }
 
     void MakeFishes()
     {
+
         int genPosIndex = Random.Range(0, genPositions.Length);//ランダムの位置から魚を生成する
-        int fishPreIndex = Random.Range(0, fishPrafabs.Length);//ランダムに魚を生成する
-                                                             
-        int maxNum = fishPrafabs[fishPreIndex].GetComponent<FishAttr>().maxNum;  //魚の最大生成量とスピード
-        int maxSpeed = fishPrafabs[fishPreIndex].GetComponent<FishAttr>().maxSpeed;
+        int fishPreIndex = Random.Range(0, fishPrefabs.Length);//ランダムに魚を生成する
+
+        int maxNum = fishPrefabs[fishPreIndex].GetComponent<FishAttr>().maxNum;
+        int maxSpeed = fishPrefabs[fishPreIndex].GetComponent<FishAttr>().maxSpeed;
+        //生成されたそれぞれの魚の最大速度と数量を取得
         int num = Random.Range((maxNum / 2) + 1, maxNum);//生成される量が1から最大
         int speed = Random.Range(maxSpeed / 2, maxSpeed);
         int moveType = Random.Range(0, 2); //0の場合まっすぐ　2は曲がる
         int angOffset;//まっすぐの場合　x軸との角度
-        int angSpeed;//曲がる角スピード
+        int angSpeed;//曲がる角スピード 
         
         if (moveType == 0)
         {
+            angOffset = Random.Range(-22, 22);
+            // 画面に入る魚群の角度範囲 
+            GenStraightFish(genPosIndex, fishPreIndex, num, speed, angOffset);
             //まっすぐ魚群
         }
         else
@@ -30,5 +42,18 @@ public class FishMaker : MonoBehaviour
             //曲がる.
         }
     }
+    void GenStraightFish(int genPosIndex,int fishPreIndex,int num,int speed,int angOffset)
+    {
+        for (int i = 0;i<num;i++)
+        {
+            GameObject fish = Instantiate(fishPrefabs[fishPreIndex]);
+            fish.transform.SetParent(fishHolder, false);
+            fish.transform.localPosition = genPositions[genPosIndex].localPosition;
+            fish.transform.localRotation = genPositions[genPosIndex].localRotation;
+            fish.transform.Rotate(0, 0, angOffset);
+            //魚群を生成する
+            fish.AddComponent<Ef_AutoMove>().speed = speed;
+            }
 
+    }
 }
