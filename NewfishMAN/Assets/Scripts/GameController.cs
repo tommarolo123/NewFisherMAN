f25269ef3d815b23f5d53c1b27f86bc651948bb6
tommarolo123 <tommarolo123@yahoo.co.jp>
2020-03-05 15:56:08 +0900

    弾のデザイン、マウスで鉄砲を切り替え

---
+++ NewfishMAN/Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Text oneShootCostText; //一発の金額cost
+    public GameObject[] gunGos;
+    private int costIndex = 0;
+    //今使っている弾
+    private int[] oneShootCosts  = { 5,10,20,30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000};//一発かかるゴールド
+
+    void Update()
+    {
+        ChangeBulletCost();
+    }
+    void ChangeBulletCost()//マウスのスクロールで鉄砲を切り替え
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            OnButtonMDoown();
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            OnButtonPDown();
+        }
+    }
+    public void OnButtonPDown()
+    {
+        gunGos[costIndex / 4].SetActive(false);
+        //今の鉄砲を無効
+        costIndex++;
+        costIndex = (costIndex > oneShootCosts.Length - 1) ? 0 : costIndex; //最大鉄砲の後は0に戻る
+        gunGos[costIndex / 4].SetActive(true);
+        //次の鉄砲
+        oneShootCostText.text = "$" + oneShootCosts[costIndex];
+    }
+
+    public void OnButtonMDoown()
+    {
+        gunGos[costIndex / 4].SetActive(false);
+        //今の鉄砲を無効
+        costIndex--;
+        costIndex = (costIndex < 0) ? oneShootCosts.Length-1 : costIndex; //最大鉄砲の後は0に戻る
+        gunGos[costIndex / 4].SetActive(true);
+        //次の鉄砲
+        oneShootCostText.text = "$" + oneShootCosts[costIndex];
+    }
+
+
+}
