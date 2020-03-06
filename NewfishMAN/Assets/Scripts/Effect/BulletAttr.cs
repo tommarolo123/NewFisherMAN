5a265dc6149e196771c84f9a46e7c887b68554ec
tommarolo123 <tommarolo123@yahoo.co.jp>
2020-03-07 00:46:34 +0900

    綱の生成

--- NewfishMAN/Assets/Scripts/Effect/BulletAttr.cs
+++ NewfishMAN/Assets/Scripts/Effect/BulletAttr.cs
@@ -7,8 +7,24 @@ public class BulletAttr : MonoBehaviour
 
     public int speed;//弾のスピード
     public int damage;//威力
-     
+    public GameObject webPrefab;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag=="Border")
+        {
+            Destroy(gameObject); //boderに当たったら
+
+        }
+        if (collision.tag == "Fish")//魚に当たったら
+        {
+            GameObject web = Instantiate(webPrefab);//綱生成
+            web.transform.SetParent(gameObject.transform.parent,false);//弾と同じ
+            web.transform.position = gameObject.transform.position;//位置
+            
+            Destroy(gameObject);
+        }
+    }
 
 
 }
